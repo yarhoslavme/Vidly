66714eef3eba928ac56d67fca571daaa489abbb7
Yarhoslav <yarhoslavme@gmail.com>
2020-11-13 20:10:57 -0500

    Added Movies (MVC)

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie { Name = "Shrek!" };
+            return View(movie);
+        }
+    }
+}
