2b8e1194c9587f0072670eb6f196f8b5129fb7c2
Yarhoslav <yarhoslavme@gmail.com>
2020-11-14 22:27:33 -0500

    Added EntityFramework vidlycontext

---
+++ Vidly/DAL/VidlyContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Entity;
+using Vidly.Models;
+
+namespace Vidly.DAL
+{
+    public class VidlyContext : DbContext
+    {
+
+        public VidlyContext() : base("vidlycontext")
+        {
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+
+    }
+}
